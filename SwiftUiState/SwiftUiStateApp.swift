c514862a34921f51cbfe91e69263b51bfb292958
Your mato <you@example.com>
2020-12-29 22:09:26 +0800

    Initial Commit

---
+++ SwiftUiState/SwiftUiStateApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUiStateApp.swift
+//  SwiftUiState
+//
+//  Created by kittawat phuangsombat on 2020/12/29.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUiStateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
